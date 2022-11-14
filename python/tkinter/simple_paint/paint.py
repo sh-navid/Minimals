a67d5c8f68e7700647d291752f8a065d516d869d
sh-navid <shamshirsaz.navid@live.com>
2022-11-14 14:39:41 +0330

    Update

--- python/tkinter/simple_paint/paint.py
+++ python/tkinter/simple_paint/paint.py
@@ -29,17 +29,17 @@ btn_new.grid(row=0, column=0)
 
 
 btn_color = Button(text="Color", command=change_color)
-btn_color.grid(row=0, column=1)
+btn_color.grid(row=0, column=2)
 
 
-btn_size = Scale(orient=HORIZONTAL,from_=2,to=50)
-btn_size.grid(row=0, column=2)
+btn_size = Scale(orient=HORIZONTAL, from_=2, to=50)
+btn_size.grid(row=0, column=1)
 
 
 canvas = Canvas(background="white")
 canvas.grid(row=1, columnspan=3)
 
 
-win.bind("<ButtonPress-1>", mouse_click)
-win.bind("<B1-Motion>", mouse_move)
+canvas.bind("<ButtonPress-1>", mouse_click)
+canvas.bind("<B1-Motion>", mouse_move)
 win.mainloop()
