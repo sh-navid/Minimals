78751613ce3b355f67de5116d05c6e18f3d8e62f
sh-navid <shamshirsaz.navid@live.com>
2022-11-14 14:37:14 +0330

    Update

--- python/tkinter/simple_paint/paint.py
+++ python/tkinter/simple_paint/paint.py
@@ -1,12 +1,7 @@
 from tkinter import *
+from tkinter.colorchooser import askcolor
 
-x, y = None, None
-
-
-win = Tk()
-
-canvas = Canvas()
-canvas.pack()
+x, y, color = None, None, "#212121"
 
 
 def mouse_click(e):
@@ -16,8 +11,33 @@ def mouse_click(e):
 
 def mouse_move(e):
     global x, y, c
-    canvas.create_line(x, y, e.x, e.y, width=5, fill="yellow")
+    canvas.create_line(x, y, e.x, e.y, width=5, fill=color)
     x, y = e.x, e.y
+
+
+def change_color():
+    global color
+    color = askcolor()[1]
+
+
+win = Tk()
+win.title("Paint")
+
+
+btn_new = Button(text="New")
+btn_new.grid(row=0, column=0)
+
+
+btn_color = Button(text="Color", command=change_color)
+btn_color.grid(row=0, column=1)
+
+
+btn_size = Scale(orient=HORIZONTAL,from_=2,to=50)
+btn_size.grid(row=0, column=2)
+
+
+canvas = Canvas(background="white")
+canvas.grid(row=1, columnspan=3)
 
 
 win.bind("<ButtonPress-1>", mouse_click)
